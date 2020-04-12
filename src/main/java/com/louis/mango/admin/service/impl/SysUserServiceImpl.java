df3bccc98e1e93c1d454c6ec57e051dfbd83c4e8
huangpenghui <huangpenghui68@163.com>
2020-04-12 15:07:49 +0800

    aa

--- src/main/java/com/louis/mango/admin/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/louis/mango/admin/service/impl/SysUserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.louis.mango.admin.service.impl;
 
+import com.louis.mango.admin.entity.SysMenu;
 import com.louis.mango.admin.entity.SysUser;
+import com.louis.mango.admin.mapper.SysMenuMapper;
 import com.louis.mango.admin.mapper.SysUserMapper;
 import com.louis.mango.admin.service.SysUserService;
 import com.mango.mango.common.utils.DateTimeUtils;
@@ -18,12 +20,16 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class SysUserServiceImpl implements SysUserService {
 
     @Autowired
     private SysUserMapper sysUserMapper;
+    @Autowired
+    private SysMenuMapper menuMapper;
 
     public static File createUserExcelFile(List<?> records) {
         if (records == null) {
@@ -69,6 +75,13 @@ public class SysUserServiceImpl implements SysUserService {
             row.getCell(++columnIndex).setCellValue(DateTimeUtils.getDateTime(user.getLastUpdateTime()));
         }
         return PoiUtils.createExcelFile(workbook, "download_user");
+    }
+
+    @Override
+    public Set<String> findPermission(String username) {
+        List<SysMenu> sysMenus = menuMapper.findByUserName(username);
+        Set<String> permissions = sysMenus.stream().map(SysMenu::getPerms).collect(Collectors.toSet());
+        return permissions;
     }
 
     @Override
